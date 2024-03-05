48df87c564a320d2f1a4e2f88a54b78e48834629
santhoshraghuls <santhosh.raghul@kanini.com>
2024-03-05 16:21:23 +0530

    Sample NodeJs codes

---
+++ app.js
@@ -0,0 +1,2 @@
+const fs = require('fs');
+fs.writeFileSync("app.txt","Hi Welcome to NodeJs course");
---
+++ utils.js
@@ -0,0 +1,11 @@
+const obj = {
+    Name:"Santhosh Raghul",
+    Age:23
+}
+
+const func = (a,b)=>a+b;
+
+module.exports = {
+    obj,
+    func
+}
